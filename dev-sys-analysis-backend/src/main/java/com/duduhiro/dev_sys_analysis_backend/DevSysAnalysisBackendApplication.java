586bae6d4fc45dce58ae14bf1d1ab395095296e4
duduhiro0312@gmail.com <duduhiro0312@gmail.com>
2025-03-09 19:34:08 -0300

    feat: initialize backend project structure with Spring Boot

---
+++ dev-sys-analysis-backend/src/main/java/com/duduhiro/dev_sys_analysis_backend/DevSysAnalysisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.duduhiro.dev_sys_analysis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSysAnalysisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSysAnalysisBackendApplication.class, args);
+	}
+
+}
---
+++ dev-sys-analysis-backend/src/test/java/com/duduhiro/dev_sys_analysis_backend/DevSysAnalysisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duduhiro.dev_sys_analysis_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSysAnalysisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
